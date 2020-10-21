3bb9dea06b4e9600fd675da964b269bf218ea1fe
hankel343 <57358008+hankel343@users.noreply.github.com>
2020-10-21 16:33:00 -0500

    Functional copy lacking some features.
    Add:
    -How many games have been played
    -How many games have been won
    -How many games have been lost
    -Fix output spacing

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -1,99 +1,132 @@
 #include <iostream>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
-int Success_Message(int ranNum);
-int Menu();
-int New_Game();
+void Wrong_Guess();
+void Success_Message();
+bool New_Game_Menu();
 
-int main()
-{
-	if (New_Game() == 1)
-	{
-		return 1; // terminates program
-	}
-	else
-		New_Game();
+int main() {
+	bool gameLoop = true;
+	bool programLoop = true;
 
+
+	//Outer do while controls if a new game is executed or not with a boolean value controled do-while conditon.
+	do {
+
+		int numOfGuesses = 1; //Resets guess count to one every iteration.
+		int ranNum{}; //Random number is reinitialized to guarantee a new random number from the last game.
+		int guess{}; //Stores user's guess.
+
+		srand(unsigned int(time(NULL))); //Seeds random number generator
+		ranNum = rand() % 10 + 1; //Generates random numbers between 0 and 100
+
+		cout << "The computer has generated a random number between 0 and 100 inclusive\n";
+		cout << "You have 20 tries to guess the random number.\n";
+		cout << "Enter your first guess: ";
+		cin >> guess;
+
+		gameLoop = true; //Allows for repeat games
+		//This while loop represents one game.
+		while (numOfGuesses <= 20 && gameLoop) {
+			if (guess != ranNum) {
+				Wrong_Guess();
+				cout << "Try again: ";
+				cin >> guess;
+			}
+			else {
+				Success_Message();
+				if (New_Game_Menu() == 1) {
+					gameLoop = false;
+					programLoop = true;
+				}
+				else {
+					gameLoop = false;
+					programLoop = false;
+				}
+			}
+		}
+		if (numOfGuesses > 20) {
+			cout << "Sorry, you're out of guesses!\n\n";
+			if (New_Game_Menu() == 1) {
+				programLoop = true;
+			}
+			else
+				programLoop = false;
+		}
+	} while (programLoop); //Starts new game if true - ends program if false.
+
+	cout << "\n\nGoodbye...\n\n";
 
 	return 0;
 }
 
-int New_Game()
-{
-	int ranNum;
-	int numOfGuesses{}; //initialized to zero.
-	int guess{}; //initialized to zero before user input.
+void Wrong_Guess() {
+	int failureMessageIndex{}; //Used to generate random success messages in the if-else ladder below
 
 	srand(unsigned int(time(NULL))); //Seeds random number generator
-	ranNum = rand() % 10 + 1; //Generates random numbers between 0 and 100
+	failureMessageIndex = rand() % 4 + 1; //Generates random numbers between 0 and 5
 
-	cout << "\n\n\nThe computer has generated a random number between 0 and 100\n";
-	cout << "You have 20 guesses to correctly guess what the random number is\n\n";
-
-	cout << "Enter your first guess: ";
-	cin >> guess;
-
-	while (numOfGuesses < 20)
-	{
-		if (guess == ranNum)
-		{
-			if (Success_Message(ranNum) == 0)
-			{
-				New_Game();
-			}
-			else if (Success_Message(ranNum) == 1)
-			{
-				return 1;
-			}
-		}
-
-		cout << "Nope - that's not the random number.\n";
-
-		cout << "Try again: ";
-		cin >> guess;
-		++numOfGuesses;
+	if (failureMessageIndex == 1) {
+		cout << "Nope\n";
 	}
-
-	cout << "\n\nYou lose!";
-}
-
-int Success_Message(int ranNum)
-{
-	cout << "You win!\n The secret number was " << ranNum << endl;
-
-	if (Menu() == 0)
-	{
-		return 0;
+	else if (failureMessageIndex == 2) {
+		cout << "That's not it.\n";
 	}
-	else if (Menu() == 1)
-	{
-		return 1;
+	else if (failureMessageIndex == 3) {
+		cout << "Keep trying.\n";
+	}
+	else if (failureMessageIndex == 4) {
+		cout << "Keep going\n";
+	}
+	else if (failureMessageIndex == 5) {
+		cout << "Tough luck this time\n";
 	}
 }
 
-int Menu()
-{
-	char selection;
+void Success_Message() {
+	int successMessageIndex{}; //Used to generate random failure messages in the if-else ladder below
 
-	cout << "\n\n\nSelect one of the following: \n";
+	srand(unsigned int(time(NULL))); //Seeds random number generator
+	successMessageIndex = rand() % 4 + 1; //Generates random numbers between 0 and 5
 
-	do
-	{
-		cout << "New Game - 1\n";
-		cout << "Quit - 2\n";
-		cout << "Enter your choice: ";
-		cin >> selection;
+	if (successMessageIndex == 1) {
+		cout << "Good job!\n";
+	}
+	else if (successMessageIndex == 2) {
+		cout << "That's it!\n";
+	}
+	else if (successMessageIndex == 3) {
+		cout << "You got it right!\n";
+	}
+	else if (successMessageIndex == 4) {
+		cout << "Nice!\n";
+	}
+	else if (successMessageIndex == 5) {
+		cout << "Excelsior\n";
+	}
+}
 
-		switch (selection)
-		{
-		case '1':	return 0;
-			break;
+bool New_Game_Menu() {
+	char choice; //Stores user's selection in menu
+	bool newGame{}; //Stores return value of function - used to determine if new game is initiated or user terminates program. 
 
-		case '2':	return 1;
-			break;
-		}
-	} while(selection != 2);
+	cout << "\n\n\nPlease make a selection from the menu below:\n";
+	cout << "New Game - 1\n";
+	cout << "Quit Program - 2\n\n";
+
+	cout << "Enter your choice: ";
+	cin >> choice;
+
+	switch (choice) {
+	case '1':	newGame = true;
+		break;
+
+	case '2':	newGame = false;
+		break;
+	}
+
+	return newGame;
 }
